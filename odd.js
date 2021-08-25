fab8b780e0e79eb11e3b91d8b5eaa432b7ec2eb6
Karen Tolentino <k.tolentino@live.ca>
2021-08-25 18:44:02 +0000

    Odd checker

---
+++ odd.js
@@ -0,0 +1,12 @@
+const isOdd = function (num) {
+  if (num % 2 != 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+isOdd(3);
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));

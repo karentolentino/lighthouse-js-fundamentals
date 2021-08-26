a9a3e862c252adbc4ad43e3e443e6b37d8916087
Karen Tolentino <k.tolentino@live.ca>
2021-08-26 17:54:46 +0000

    ingredients.js

--- ingredients.js
+++ ingredients.js
@@ -11,3 +11,8 @@ console.log("Banana Bread Ingredients: ");
 for (let i = 0; i < ingredients.length; i++) {
   console.log(ingredients[i]);
 }
+
+console.log("Banana Bread Ingredients: ");
+for (let i = ingredients.length - 1; i >=0; i--) {
+  console.log(ingredients[i]);
+}
